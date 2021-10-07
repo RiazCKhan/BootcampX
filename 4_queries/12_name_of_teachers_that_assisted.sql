15ff3f1e98d029ebf5f7e45f01e8711662375da9
RiazK <RiazCKhan@gmail.com>
2021-10-07 09:45:42 -0400

    Add: name of teachers that assisted

---
+++ 4_queries/12_name_of_teachers_that_assisted.sql
@@ -0,0 +1,18 @@
+SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY cohorts.name;
+
+/* COMPASS ANSWER */
+
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher;
